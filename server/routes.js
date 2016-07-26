cf980d56301c7146f4103ced6d4241fa6863c011
studio.gameolive <studio.gameolive@gmail.com>
2016-07-26 21:59:49 +0000

    Update Build

--- server/routes.js
+++ server/routes.js
@@ -18,6 +18,8 @@ var _path = require('path');
 
 var _path2 = _interopRequireDefault(_path);
 
+var swaggerJSDoc = require('swagger-jsdoc');
+
 exports['default'] = function (app) {
   // Insert routes below
   app.use('/api/users', require('./api/user'));
@@ -29,8 +31,30 @@ exports['default'] = function (app) {
   app.use('/api/gameRounds', require('./api/gameRound'));
   app.use('/api/wallet', require('./api/wallet'));
 
+  var options = {
+    swaggerDefinition: {
+      info: {
+        title: 'Game Server Platform', // Title (required)
+        version: '1.0.0' }
+    },
+    // Version (required)
+    apis: ['./routes.js'] };
+
+  // Initialize swagger-jsdoc -> returns validated swagger spec in json format
+  // Path to the API docs
+  var swaggerSpec = swaggerJSDoc(options);
+  app.get('/api/docs.json', function (req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(swaggerSpec);
+  });
+
   // All undefined asset or api routes should return a 404
   app.route('/:url(api|auth|components|app|bower_components|assets)/*').get(_componentsErrors2['default'][404]);
+
+  // All other routes should redirect to the index.html
+  app.route('/docs').get(function (req, res) {
+    res.sendFile(_path2['default'].resolve(app.get('appPath') + '/swagger-ui/index.html'));
+  });
 
   // All other routes should redirect to the index.html
   app.route('/*').get(function (req, res) {
